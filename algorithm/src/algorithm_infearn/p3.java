6cb3f0d6635e3ec63fe3e31cf04699396e01f79f
baejihyeon <wlgus6343@naver.com>
2022-04-24 16:26:43 +0900

    [Solve] ✍ 3.문장 속 단어

--- algorithm/src/algorithm_infearn/p3.java
+++ algorithm/src/algorithm_infearn/p3.java
@@ -1,5 +1,25 @@
 package algorithm_infearn;
 
-public class p3 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class p3 {
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    String input1 = in.nextLine();
+	    String[] tmp;
+	    
+	    String answer ="";
+		tmp = input1.split(" ");   
+		int max = tmp[0].length();
+	    for(int i=0; i<tmp.length; i++) {
+	    	if(max < tmp[i].length()) {
+	    		answer = tmp[i];
+	    	}
+	
+	    } System.out.println(answer);
+	}	
 }
+//list length 구해서 max 구하기 
+
