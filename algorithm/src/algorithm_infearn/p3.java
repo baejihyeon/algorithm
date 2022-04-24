e8fbde5751e1501b3f5c34cbd25610457b641d59
배지현 <51259368+baejihyeon@users.noreply.github.com>
2022-04-24 16:50:41 +0900

    Update p3.java

--- algorithm/src/algorithm_infearn/p3.java
+++ algorithm/src/algorithm_infearn/p3.java
@@ -1,54 +1,27 @@
 package algorithm_infearn;
 import java.util.Scanner;
 
-<<<<<<< HEAD
 public class p3 {
 	public static void main(String[] args) {
 	    Scanner in=new Scanner(System.in);
 	    String input1 = in.nextLine();
 	    String[] tmp;
+
 		tmp = input1.split(" ");
 		
 		String max = tmp[0];   
 		
 		for(int i=0; i<tmp.length; i++) {
-=======
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
->>>>>>> branch 'main' of https://github.com/baejihyeon/algorithm.git
 
-<<<<<<< HEAD
 	    	if(max.length() < tmp[i].length()) {
 	    		max = tmp[i];
 	    	}
 	    } System.out.println(max);
-=======
-public class p3 {
-	public static void main(String[] args) {
-	    Scanner in=new Scanner(System.in);
-	    String input1 = in.nextLine();
-	    String[] tmp;
-	    
-	    String answer ="";
-		tmp = input1.split(" ");   
-		int max = tmp[0].length();
-	    for(int i=0; i<tmp.length; i++) {
-	    	if(max < tmp[i].length()) {
-	    		answer = tmp[i];
-	    	}
-	
-	    } System.out.println(answer);
->>>>>>> branch 'main' of https://github.com/baejihyeon/algorithm.git
 	}	
 }
-//list length ±¸ÇØ¼­ max ±¸ÇÏ±â 
-<<<<<<< HEAD
+//list length êµ¬í•´ì„œ max êµ¬í•˜ê¸° 
 //System.out.println(Arrays.toString(tmp));
 //System.out.println(tmp[i].length());
 //else if(tmp.length == 1) {
 //answer = tmp[0];
 //}
-=======
-
->>>>>>> branch 'main' of https://github.com/baejihyeon/algorithm.git
